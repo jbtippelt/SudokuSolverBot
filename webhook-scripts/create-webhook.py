f8d562c6730e948b2759686612336c0e15a83e42
Jakob Tippelt <jboka@jtmac.fritz.box>
2018-12-16 18:25:10 +0100

    Updated webhook-scripts to use env variables

--- webhook-scripts/create-webhook.py
+++ webhook-scripts/create-webhook.py
@@ -7,22 +7,12 @@ CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
-#The environment name for the beta is filled below. Will need changing in future		
-ENVNAME = 'prod' #os.environ.get('ENVNAME', None)
-WEBHOOK_URL = sys.argv[1]
+ENV_LABEL = os.environ.get('ENV_LABEL')
+WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
 
 twitterAPI = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-r = twitterAPI.request('account_activity/all/:prod/webhooks', {'url': WEBHOOK_URL})
+r = twitterAPI.request('account_activity/all/:%s/webhooks' % ENV_LABEL, {'url': WEBHOOK_URL})
 
 print (r.status_code)
 print (r.text)
-
-
-'''
-
-
-curl --request POST --url 'https://api.twitter.com/1.1/account_activity/all/:ENV_NAME/webhooks.json?url=https%3A%2F%2Feddie.jboka.de%2Fsuso%2Fwebhook%2Ftwitter' --header 'authorization: OAuth oauth_consumer_key="lmNQ13LTSKWX8Jc5iybszJkG3", oauth_nonce="da39a3ee5e6b4b0d3255bfef95601890afd80709", oauth_signature="GENERATED", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1544222595", oauth_token="1070291557872558080-6voJmD0BP362jrVAXCl51KGUeLnald", oauth_version="1.0"'
-
-
-'''
--- webhook-scripts/subscribe-account.py
+++ webhook-scripts/subscribe-account.py
@@ -7,12 +7,11 @@ CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
-ENVNAME = 'prod' #os.environ.get('ENVNAME', None)
+ENV_LABEL = os.environ.get('ENV_LABEL')
 
 twitterAPI = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-r = twitterAPI.request('account_activity/all/:prod/subscriptions', None, None, 'POST')
+r = twitterAPI.request('account_activity/all/:%s/subscriptions' % ENV_LABEL, None, None, 'POST')
 
-#TODO: check possible status codes and convert to nice messages
 print (r.status_code)
        
