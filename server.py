7fac5b5ec4d1c176d9ae0f674af33f3b021e47a6
Jakob Tippelt <jboka@jtmac.fritz.box>
2018-12-16 18:31:09 +0100

    Updated server.py comments

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ CURRENT_USER_ID = int(os.environ.get('CURRENT_USER_ID'))
 app = Flask(__name__)	 
 
 #The GET method for webhook should be used for the CRC check
-#TODO: add header validation (compare_digest https://docs.python.org/3.6/library/hmac.html)
 @app.route("/webhook", methods=["GET"])
 def twitterCrcValidation():
     
@@ -29,35 +28,31 @@ def twitterCrcValidation():
     return json.dumps(response)   
         
 #The POST method for webhook should be used for all other API events
-#TODO: add event-specific behaviours beyond Direct Message and Like
 @app.route("/webhook", methods=["POST"])
 def twitterEventReceived():
 	  		
     requestJson = request.get_json()
-
-    #dump to console for debugging purposes
-    #print(json.dumps(requestJson, indent=4, sort_keys=True))
             
     if 'tweet_create_events' in requestJson.keys():
-        #Tweet Favourite Event, process that
+        #Tweet Create Event
         likeObject = requestJson['tweet_create_events'][0]
         userId = likeObject.get('user', {}).get('id')
         username = likeObject.get('user', {}).get('screen_name')
         twtId = likeObject.get('id_str')          
 
         
-        #event is from myself so ignore (Favourite event fires when you send a DM too)   
+        #event is from myself so ignore  
         if userId == CURRENT_USER_ID:
             print("Received own tweet with tweet-id " + twtId)
             return ('', HTTPStatus.OK)
 
+        #someone replyed to my reply
         if likeObject.get('in_reply_to_status_id'):
             print("Received a reply to tweet-id " + twtId)
             return ('', HTTPStatus.OK)
 
         print("Received tweet from" + username + " with tweet-id " + twtId)
         main.solveTweet(likeObject, True)           
-        #Twitter.processLikeEvent(likeObject)
                 
     else:
         #Event type not supported
